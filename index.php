50dc31a6f10f11fe92ec6075b1e7011876a85f06
Dmitry Kharlamov <newdimaak@gmail.com>
2019-07-24 22:08:06 +0300

    task 2-4

--- index.php
+++ index.php
@@ -4,6 +4,23 @@ require('src/functions.php');
 echo '<pre>';
 
 echo task1('data.xml');
-//task2();
-//task3();
-//task4();
+
+$cars = [
+    'opel' => [
+        'year' => 2015,
+    ],
+    'toyota' => [
+        'year' => 2017,
+    ],
+    'bmw' => [
+        'year' => 2019,
+    ],
+];
+
+task2($cars);
+
+echo task3('nums-csv.csv') . PHP_EOL;
+
+$dataArticle = task4('https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json');
+
+echo implode($dataArticle, ', ');
--- src/functions.php
+++ src/functions.php
@@ -18,30 +18,97 @@ function task1($fileName)
 
     $output .= "Order: {$orderNumber} / {$orderDate}" . PHP_EOL . PHP_EOL;
 
-    $output = outputData($addressList, $output);
+    foreach ($addressList as $address) {
+        $addressAttr = $address->attributes();
 
-    $output .= "Notes: {$orderNotes}" . PHP_EOL . PHP_EOL;
+        $output .= "Address data:" . PHP_EOL . PHP_EOL;
 
-    $output = outputData($orderList, $output);
-
-    return $output;
-}
-
-function outputData($array, $output) {
-    foreach ($array as $item) {
-        $itemAttr = $item->attributes();
-
-        $output .= "Product data:" . PHP_EOL . PHP_EOL;
-
-        foreach ($itemAttr as $key => $val) {
-            $output .= "{$key}: {$val}." . PHP_EOL;
+        foreach ($addressAttr as $attrKey => $attrVal) {
+            $output .= $attrKey . ': ' . $attrVal . '.' . PHP_EOL;
         }
 
-        foreach ($item as $key => $val) {
-            $output .= "{$key}: {$val}." . PHP_EOL;
+        foreach ($address as $addressKey => $addressVal) {
+            $output .= $addressKey . ': ' . $addressVal . '.' . PHP_EOL;
         }
 
         $output .= PHP_EOL;
     }
+
+    $output .= "Notes: {$orderNotes}" . PHP_EOL . PHP_EOL;
+
+    foreach ($orderList as $order) {
+        $orderAttr = $order->attributes();
+
+        $output .= "Product data:" . PHP_EOL . PHP_EOL;
+
+        foreach ($orderAttr as $attrKey => $attrVal) {
+            $output .= $attrKey . ': ' . $attrVal . '.' . PHP_EOL;
+        }
+
+        foreach ($order as $orderKey => $orderVal) {
+            $output .= "{$orderKey}: {$orderVal}." . PHP_EOL;
+        }
+
+        $output .= PHP_EOL;
+    }
+
     return $output;
+}
+
+function task2($data)
+{
+    file_put_contents('output.json', json_encode($data));
+
+    $isChange = rand(0, 1);
+
+    if ($isChange) {
+        $newCar = [
+            'kia' => [
+                'year' => 2013
+            ]
+        ];
+
+        $output = json_decode(file_get_contents('output.json'), true);
+        file_put_contents('output2.json', json_encode($output + $newCar));
+        $output2 = json_decode(file_get_contents('output2.json'), true);
+
+        print_r(array_diff_assoc($output2, $output));
+    }
+}
+
+function task3($file)
+{
+    $arrayNums = [];
+    for ($i = 1; $i <= 50; $i++) {
+        $arrayNums[$i] = rand(1, 100);
+    }
+
+    $csvFile = fopen($file, 'w');
+    fputcsv($csvFile, $arrayNums);
+    fclose($csvFile);
+
+    $handle = fopen($file, 'r');
+
+    $sumEvanNums = 0;
+    while (($data = fgetcsv($handle, 1000, ',')) !== FALSE) {
+        foreach ($data as $number) {
+            if ($number % 2 == 0) {
+                $sumEvanNums += $number;
+            }
+        }
+    }
+    return $sumEvanNums;
+}
+
+function task4($url)
+{
+    $json = json_decode(file_get_contents($url), true);
+
+    $data = $json['query']['pages'][15580374];
+    $output = [
+        'pageid' => $data['pageid'],
+        'title' => $data['title']
+    ];
+
+     return $output;
 }
